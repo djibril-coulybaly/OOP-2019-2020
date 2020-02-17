2ce3990ddea4ba63f4cceca39288d7273a51b0ae
Djibril Coulybaly <C18423664@mydit.ie>
2020-02-17 11:11:09 +0000

    recent changes

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -12,23 +12,45 @@ public class HelloProcessing extends PApplet
 	public void setup() {
 	}
 
+	public void draw()
+	{
+		background(255,0,0);//rgb value for background colour
+
+		noStroke();//no outline of shapes
+		fill(255,255,0);// filling the colour of all shapes after this call
+		ellipse(250, 300, 400, 397);//cx,cy, w, h circle 
+		
+		noStroke();//no outline of shapes
+		fill(0, 255, 255);
+		triangle(50, 450, 250, 30, 450, 450);//x,y x,y x,y co-ordinates for each vertices 
+	
+		noStroke();
+		fill(200, 200, 200);
+		ellipse(250, 250, 200, 100);
+
+		noStroke();
+		fill(0, 0, 0);
+		ellipse(250, 250, 70, 70);
+
+	}
+
 	/*
 	public void draw() {
-		background(0, 0, 255);
-		stroke(255, 0, 0);
-		fill(255, 255, 0);
+		background(0, 0, 255); //wipes the screen
+		stroke(255, 0, 0); //changes the outline colour for each shape
+		fill(255, 255, 0); //fills the shape
 		line(10, 10, 100, 100);		// x1, y1, x2, y2
 		point(50, 50); // x, y
 		ellipse(100, 200, 50, 150); // cx, cy, w, h
 		fill(0, 255, 0);
 		rect(50, 10, 100, 200); // tlx, tly, w, h
-		triangle(10, 10, 100, 90, 200, 200); // vertices	
+		triangle(10, 10, 100, 90, 200, 200); // vertices (x1,y1) (x2, y2) (x3, y3)	
 	}	
 	*/
 
 	int x = 0; // 4 bytes
 	
-	public void draw()
+	/*public void draw()
 	{	
 		
 		background(0);
@@ -36,5 +58,5 @@ public class HelloProcessing extends PApplet
 		fill(255);
 		ellipse(x, mouseY, 50, 50);		
 		x ++;
-	}
+	}*/
 }
--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -11,7 +11,7 @@ public class Loops extends PApplet
 
 	public void setup() 
 	{
-		
+		colorMode(HSB);
 	}
 
 	
@@ -26,25 +26,88 @@ public class Loops extends PApplet
 
 	public void draw()
 	{	
-		background(0);		
-		stroke(255);
-		line(10, 10, 100, 10);
-		line(10, 20, 100, 20);
-		line(10, 30, 100, 30);
-		line(10, 40, 100, 40);
+		// background(0);		
+		// stroke(255);
+		// line(10, 10, 100, 10);
+		// line(10, 20, 100, 20);
+		// line(10, 30, 100, 30);
+		// line(10, 40, 100, 40);
 
-		for(int y = 10 ; y <= 40 ; y += 10)
-		{
-			line(200, y, 300, y);
-		}
+		// for(int y = 10 ; y <= 40 ; y += 10)
+		// {
+		// 	line(200, y, 300, y);
+		// }
 
-		int yy = 10;
-		while(yy <= 40)
-		{
-			line(300, yy, 400, yy);
-			yy += 10;
+		// int yy = 10;
+		// while(yy <= 40)
+		// {
+		// 	line(300, yy, 400, yy);
+		// 	yy += 10;
+		// }
+		
+		//picture1();
+		//picture2();
+		picture3();
+	}
+
+	public void picture1() {
+		background(244,244,244);
+		int x1 = 0;
+		int y1 = 0;
+		int w = width;
+		int h = height;
+		int xcentre = (width/2);
+		int ycentre = (height/2); 
+		
+
+		for (y1 = 0; y1 <= h; y1 += 20) {
+			stroke(0,0,0);
+			line(x1, y1, xcentre, ycentre);
 		}
 		
-	
+		for (x1 = 0; x1 <= w; x1 += 20) {
+			stroke(0,0,0);
+			line(x1, y1, xcentre, ycentre);
+		}
+
+		for (; y1 > h-h; y1 -= 20) {
+			stroke(0,0,0);
+			line(x1, y1, xcentre, ycentre);
+		}
+
+		for (; x1 > w-w; x1 -= 20) {
+			stroke(0,0,0);
+			line(x1, y1, xcentre, ycentre);
+		}
+	}
+
+	public void picture2() {
+		int w = width;
+		int h = height;
+		int x = 25;
+		int y = 50;
+
+		background(244,244,244);
+
+		for (; x < w; x += 50){
+			fill(255,255,255);
+			ellipse(x, y, 50, 50);
+		}
+		
+
+	}
+
+	public void picture3() {
+		int w = width;
+		int h = height;
+
+		background(244,244,244);
+
+		for (; x < w; x += 50){
+			fill(255,255,255);
+			ellipse(x, y, 50, 50);
+		}
+		
+
 	}
 }
