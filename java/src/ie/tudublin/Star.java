b39d133d24d901e00a24974f809d9568382f7a26
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2020-03-09 15:20:02 +0000

    Getting recent updates from master branch

--- java/src/ie/tudublin/Star.java
+++ java/src/ie/tudublin/Star.java
@@ -1,7 +1,5 @@
 package ie.tudublin;
 
-import java.util.Map;
-
 import processing.core.PApplet;
 import processing.data.TableRow;
 
@@ -116,15 +114,19 @@ public class Star
         );
     }
 
-    public void render(PApplet pa) {
+    public void render(PApplet pa)
+    {
         float border = pa.width * 0.05f;
         float x = PApplet.map(xG, -5, 5, border, pa.width - border);
         float y = PApplet.map(yG, -5, 5, border, pa.height - border);
-
+        pa.noFill();
         pa.stroke(255, 255, 0);
-        pa.line(x, y - 5, x, y + 5 );
+        pa.line(x, y -5, x, y + 5);
         pa.line(x - 5, y, x + 5, y);
-        
+        pa.stroke(255, 0, 0);
+        pa.ellipse(x, y, absMag, absMag);
+        pa.textAlign(PApplet.LEFT, PApplet.CENTER);
+        pa.fill(255);
+        pa.text(displayName, x + 50, y);
     }
-    
 }
